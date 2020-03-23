32673ee2800c35db2fa53833d65f68a539894779
Micah Banschick <micha.ban@gmail.com>
2020-03-23 20:43:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  
+  attr_accessor: :name, :type
+  attr_reader :id, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @id = id 
+    @name = name 
+    @type = type
+    @db = db 
+  end 
+  
+  def self.save
+  end 
+  
+  def self.find(id)
+  end
+  
 end
